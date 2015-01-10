9c985b197c9a313a809ceaecb7caa6a37ab2593d
Michelle <michelle.duer@gmail.com>
2015-01-09 16:26:10 -0800

    Add files

---
+++ includes/config.php
@@ -0,0 +1,30 @@
+<?php
+
+    /**
+     * config.php
+     *
+     * CityAide
+     *
+     */
+
+    // display errors, warnings, and notices
+    ini_set("display_errors", true);
+    error_reporting(E_ALL);
+
+    // requirements
+    require("constants.php");
+    require("functions.php");
+
+    // enable sessions
+    session_start();
+
+    // require authentication for most pages
+    if (!preg_match("{(?:login|logout|register|password)\.php$}", $_SERVER["PHP_SELF"]))
+    {
+        if (empty($_SESSION["id"]))
+        {
+            redirect("login.php");
+        }
+    }
+
+?>
